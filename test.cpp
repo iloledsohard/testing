2c03e15add7d0e0e630db2602fb16b26a1f573f4
Mischa Zverlov <Mischa@Mikhails-Air.fritz.box>
2017-07-16 23:32:20 +0200

    comit fuer thread

--- test.cpp
+++ test.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <thread>
 
-int main()
-{
-    std::cout << "Hallo Test" << std::endl;
+static const int num_threads = 4;
+
+//This function will be called from a thread
+
+void call_from_thread(int tid) {
+    std::cout << "Launched by the thread " << tid << std::endl;
+}
+// test2
+
+int main() {
+    std::thread t[num_threads];
+
+    //Launch a group of threads
+    for (int i = 0; i < num_threads; ++i) {
+        t[i] = std::thread(call_from_thread, i);
+    }
+
+    std::cout << "Launched from the main\n";
+
+    //Join the threads with the main thread
+    for (int i = 0; i < num_threads; ++i) {
+        t[i].join();
+    }
 
     return 0;
 }
